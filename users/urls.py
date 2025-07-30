e81f2c2a5694094a3378485a1ef5fd08caefaab6
Tamim <tamimvaiya.123@gmail.com>
2025-07-30 21:39:41 +0600

    Added Registering User Function

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_login, index
+from .views import user_login, index, register
 from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('register/',register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -23,3 +23,15 @@ def user_login(request):
 @login_required
 def index(request):
     return render(request, 'users/index.html')
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password2'])
+            new_user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'user_form': user_form})
