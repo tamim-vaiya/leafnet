d4dc29755038f317ffa4d5f070014643bf56d94c
Tamim <tamimvaiya.123@gmail.com>
2025-07-30 17:24:49 +0600

    Added Password Reset View

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import user_login, index
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-change/', PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password-change'),
     path('password-change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
+    path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password_reset'),
 ]
