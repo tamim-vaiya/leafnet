fd0039d09415310798abb07fb37f08f794645ca6
Tamim <tamimvaiya.123@gmail.com>
2025-07-30 13:28:13 +0600

    Handled POST Request for Login

--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 def user_login(request):
-    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username = data['username'], password = data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse("User authenticated and logged in!")
+            else:
+                return HttpResponse("Invalid User!")
+
+    else:
+        form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
