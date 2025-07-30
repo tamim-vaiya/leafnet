ca9465e6387b614cf3933b70d0e065c6ad06311d
Tamim <tamimvaiya.123@gmail.com>
2025-07-30 16:44:23 +0600

    Added Protected Route

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import user_login
+from .views import user_login, index
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', index, name='index')
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import LoginForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
     if request.method == "POST":
@@ -11,10 +12,14 @@ def user_login(request):
             user = authenticate(request, username = data['username'], password = data['password'])
             if user is not None:
                 login(request, user)
-                return render(request, 'users/login_success.html')
+                return render(request, 'users/index.html')
             else:
                 return HttpResponse("Invalid User!")
 
     else:
         form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
+
+@login_required
+def index(request):
+    return render(request, 'users/index.html')
