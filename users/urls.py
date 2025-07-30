60932c791cb7dec320bc95a7fd99b674fef85ec3
Tamim <tamimvaiya.123@gmail.com>
2025-07-30 17:12:31 +0600

    Added Password Change View

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import user_login, index
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns = [
+    path('', index, name='index'),
     path('login/', user_login, name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', index, name='index')
+    path('password-change/', PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password-change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
 ]
