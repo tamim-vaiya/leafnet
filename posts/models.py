3b6794d885b2f68b3334bc9926a74098533dd0de
Tamim <tamimvaiya.123@gmail.com>
2025-07-31 17:28:11 +0600

    Overwrite save function to autosave slug

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 # Create your models here.
 class Post(models.Model):
@@ -12,3 +13,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
