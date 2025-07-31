69583d960f70fdc771031ed4a0d3d1a91836ae67
Tamim <tamimvaiya.123@gmail.com>
2025-08-01 00:22:24 +0600

    Improved base.html and login.html design with tailwind

--- leafnet/settings.py
+++ leafnet/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'posts',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./users/templates/**/*.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
