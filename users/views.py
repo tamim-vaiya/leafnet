5e1aecfc404b73fc8f5c664334e337d1a7ebf36c
Tamim <tamimvaiya.123@gmail.com>
2025-07-31 20:39:37 +0600

    Added Posts on Index page

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from posts.models import Post
 
 
 def user_login(request):
@@ -24,7 +25,9 @@ def user_login(request):
 
 @login_required
 def index(request):
-    return render(request, 'users/index.html')
+    current_user = request.user
+    posts = Post.objects.filter(user=current_user)
+    return render(request, 'users/index.html', {'posts': posts})
 
 def register(request):
     if request.method == "POST":
